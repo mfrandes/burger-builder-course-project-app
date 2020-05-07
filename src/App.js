2725f50590fd0c643ed52ba23e82889f84c5ac75
Mircea <frandesmyrce@gmail.com>
2020-05-07 20:54:56 +0300

    created Layout component & first structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div >
-        
+        <Layout>
+          <p>Test Layout</p>
+        </Layout>
       </div>
     );
   }
