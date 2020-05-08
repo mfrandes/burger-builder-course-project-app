ac1005732ce6efc4bd28b61dedf8a516c9cbb3c7
Mircea <frandesmyrce@gmail.com>
2020-05-08 10:17:55 +0300

    added state property to the state class component and dinamicly output ingredients in the burger stateles component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])] //return an array with a given number of empty elements
+                .map((_, i) => {
+                    return <BurgerIngredient key={ingKey + i} type={ingKey} />
+                });
+        });
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
-            <BurgerIngredient type='bread-bottom'/>
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
         </div>
     );
 };
 
 export default burger;
+
+// transformedIngredients = is a way to transform a object in to an array of burger ingredients where the value is important fro me to 
+// decide how many ingredients we need and the key indicates what ingredient we need this is kewl stuff
