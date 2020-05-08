8927332986faaabeb388d2614b1299aaf265467a
Mircea <frandesmyrce@gmail.com>
2020-05-08 10:27:24 +0300

    Calculate the ingredients sum dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])] //return an array with a given number of empty elements
                 .map((_, i) => {
                     return <BurgerIngredient key={ingKey + i} type={ingKey} />
                 });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []); // we need to reduce the array in order to check if there are ingreidens added at all plain JS array.redice()
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start add ingredients</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
