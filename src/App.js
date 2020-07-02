4b18b0d74c42b24edf9b9032d43dcfe2dda0185b
Mircea <mircea.frandes@gloobus.it>
2020-07-02 15:44:00 +0300

    rem unused imports

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
